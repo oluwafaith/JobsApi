e8636460a5321a1cd6f2ce75dcfe52e27c6c2db4
faith okpala <okpalafaith1@gmail.com>
2022-08-16 20:00:04 +0100

    added error

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,6 +1,7 @@
 import {Request, Response} from 'express'
 import {StatusCodes} from 'http-status-codes'
 import {User} from "../models/User"
+// import {BadRequestError, UnauthenticatedError} from "../errors/index"
 const register = async (req :Request, res: Response) => {
 const user = await User.create({...req.body})
   res.status(StatusCodes.CREATED).json({user})
