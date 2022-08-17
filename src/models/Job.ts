cadc6bfa1e6c37f70b042746eeb3b6dc66c5a8d4
faith okpala <okpalafaith1@gmail.com>
2022-08-17 19:57:53 +0100

    added job schema

---
+++ src/models/Job.ts
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const jobSchema = new mongoose.Schema(
+  {
+    company: {
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
+    },
+    position: {
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
+    },
+    status: {
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+)
+export const Job = mongoose.model("Job", jobSchema);
+
