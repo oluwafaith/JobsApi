8764b9f43c824220db9c7d1e03e8612962cbe423
faith okpala <okpalafaith1@gmail.com>
2022-08-22 13:11:58 +0100

    removed files

--- src/middleware/rateLimit.ts
+++
@@ -1,9 +0,0 @@
-// import rateLimit,{RateLimitRequestHandler} from 'express-rate-limit';
-
-// const rateLimiter: RateLimitRequestHandler = rateLimit({
-//   windowMs: 15 * 60 * 1000,
-//   max: 20,
-//   skipSuccessfulRequests: true,
-// });
-
-// export default rateLimiter;
