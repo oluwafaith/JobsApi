69fb43dbe0926437a01b78460e109e216b20380a
faith okpala <okpalafaith1@gmail.com>
2022-08-22 12:47:52 +0100

    security packages added

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,11 @@ import express, { NextFunction } from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from  'morgan';
+import helmet from 'helmet';
+import cors from "cors";
+import mongoSanitize from "express-mongo-sanitize"
+import rateLimiter from 'express-rate-limit';
+
 
 import  authenticateUser from'./middleware/authentication'
 import indexRouter from './routes/index';
@@ -18,12 +23,26 @@ const app = express();
 app.set('views', path.join(__dirname,'..', 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100, 
+  })
+);
+// app.use(logger('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(mongoSanitize());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname,'..', 'public')));
 
+app.use('/api/v1', rateLimiter);
+
+app.use(express.json({ limit: '10kb' }));
 
 app.use('/', indexRouter);
 app.use('/api/v1/auth', authRouter);
---
+++ src/middleware/rateLimit.ts
@@ -0,0 +1,9 @@
+// import rateLimit,{RateLimitRequestHandler} from 'express-rate-limit';
+
+// const rateLimiter: RateLimitRequestHandler = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 20,
+//   skipSuccessfulRequests: true,
+// });
+
+// export default rateLimiter;
